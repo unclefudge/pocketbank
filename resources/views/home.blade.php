ebd403c15e132563701d7e45813a9822bd4cfb4a
= <=>
2018-02-07 23:35:21 +1100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                             @if(Auth::user()->id < 3)
                                 @foreach ([3, 4, 5] as $id)
                                     <div class="row" style="line-height: 2">
-                                        <div class="col-xs-4"><a href="{!! \App\User::find($id)->username !!}"><i class="fa fa-search" style="width: 30px"></i>{!! \App\User::find($id)->name !!}</a>
+                                        <div class="col-xs-4"><a href="{!! \App\User::find($id)->username !!}">{!! \App\User::find($id)->name !!}</a>
                                         </div>
                                         <div class="col-xs-4">{!! \App\User::find($id)->balance !!}</div>
                                         <div class="col-xs-4"><a href="/update/{{$id}}" class="btn btn-primary btn-xs">Update</a></div>
